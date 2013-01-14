356f9732350fd0ab1045fe2d3fff04e5459e1907
MinmingQian <qianminming@gmail.com>
2013-01-14 10:50:29 +0800

    change filename

---
+++ 2013/01/14(Mon-wk01).md
@@ -0,0 +1,11 @@
+#mdlearn
+    
+    1.if want to link to a url, should write the http before the domain name, if not do, it will just think it's a suburl of the current url
+    2.
+
+#TODO I need to change the name of diary
+    
+    1.show include the week of the year, and the day of the week
+    2.change the diary python script
+    
+    finished
--- diary.py
+++ diary.py
@@ -4,23 +4,24 @@ import time
 year = time.strftime("%Y")
 month = time.strftime("%m")
 day = time.strftime("%d")
+weekday = time.strftime("%a")
+weeknum = time.strftime("%W")
 
 #print year,month,day
 
-
 if not os.path.exists(year):
-	os.mkdir(year)
+    os.mkdir(year)
 
 os.chdir(year)
 #print os.getcwd()
 
 if not os.path.exists(month):
-	os.mkdir(month)
+    os.mkdir(month)
 
 os.chdir(month)
 #print os.getcwd()
 
 if os.name is "nt":
-    os.system("gvim %s.md"%(day))
+    os.system("gvim %s(%s-wk%s).md"%(day, weekday, weeknum))
 else:
-    os.system("vim %s.md"%(day))
+    os.system("vim %s(%s-wk%s).md"%(day, weekday, weeknum))
